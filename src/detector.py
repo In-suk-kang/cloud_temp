ba21cedf5c8cdc6704627a710805dcdb5864e65d
inseok <kis0062195@gmail.com>
2026-09-03 22:42:48 +0900

    Add : ebs, route_table, iam 기능 추가

--- src/detector.py
+++ src/detector.py
@@ -16,8 +16,10 @@ class GeneralAWSDriftDetector:
     def take_snapshot(self):
         snapshot = {
             'timestamp': datetime.now().isoformat(),
-            'cloudtrail': {}, 'ec2': {}, 's3': {}, 
-            'iam': {}, 'kms': {}, 'secretsmanager': {}
+            'cloudtrail': {}, 'ec2': {}, 'security_group': {},
+            'ebs_snapshot': {}, 'route_table': {},
+            's3': {}, 'iam': {}, 'iam_access_key': {},
+            'kms': {}, 'secretsmanager': {}
         }
 
         # 1. CloudTrail
@@ -28,13 +30,30 @@ class GeneralAWSDriftDetector:
                 snapshot['cloudtrail'][name] = {'is_logging': status.get('IsLogging')}
         except ClientError: pass
 
-        # 2. EC2
+        # 2. EC2, Security Group, EBS Snapshot, Route Table
         try:
             for r in self.ec2.describe_instances().get('Reservations', []):
                 for i in r.get('Instances', []):
-                    snapshot['ec2'][f"Instance:{i['InstanceId']}"] = {'state': i['State']['Name']}
+                    snapshot['ec2'][i['InstanceId']] = {'state': i['State']['Name']}
+                    
             for sg in self.ec2.describe_security_groups().get('SecurityGroups', []):
-                snapshot['ec2'][f"SG:{sg['GroupId']}"] = {'ingress': sg.get('IpPermissions', [])}
+                snapshot['security_group'][sg['GroupId']] = {
+                    'group_name': sg.get('GroupName', ''),
+                    'ingress': sg.get('IpPermissions', []),
+                    'egress': sg.get('IpPermissionsEgress', [])
+                }
+                
+            for snap in self.ec2.describe_snapshots(OwnerIds=['self']).get('Snapshots', []):
+                snapshot['ebs_snapshot'][snap['SnapshotId']] = {
+                    'volume_id': snap.get('VolumeId', ''),
+                    'state': snap.get('State', '')
+                }
+                
+            for rt in self.ec2.describe_route_tables().get('RouteTables', []):
+                snapshot['route_table'][rt['RouteTableId']] = {
+                    'vpc_id': rt.get('VpcId', ''),
+                    'routes': rt.get('Routes', [])
+                }
         except ClientError: pass
 
         # 3. S3
@@ -50,10 +69,19 @@ class GeneralAWSDriftDetector:
                 snapshot['s3'][b_name] = b_info
         except ClientError: pass
 
-        # 4. IAM
+        # 4. IAM & Access Key
         try:
             for user in self.iam.list_users().get('Users', []):
-                snapshot['iam'][f"User:{user['UserName']}"] = {'created': user['CreateDate'].isoformat()}
+                user_name = user['UserName']
+                snapshot['iam'][f"User:{user_name}"] = {'created': user['CreateDate'].isoformat()}
+                
+                keys = self.iam.list_access_keys(UserName=user_name).get('AccessKeyMetadata', [])
+                for key in keys:
+                    snapshot['iam_access_key'][key['AccessKeyId']] = {
+                        'user': user_name,
+                        'status': key['Status']
+                    }
+                    
             for role in self.iam.list_roles().get('Roles', []):
                 snapshot['iam'][f"Role:{role['RoleName']}"] = {'assume_role_policy': role.get('AssumeRolePolicyDocument')}
         except ClientError: pass
