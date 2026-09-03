c6bdd3f6fd0d3c56f8d7cb1a72693c40bee8edb7
inseok <kis0062195@gmail.com>
2026-09-03 22:43:54 +0900

    생성 json 파일 data 하위로 수정

--- main.py
+++ main.py
@@ -8,30 +8,22 @@ if __name__ == "__main__":
     
     detector = GeneralAWSDriftDetector()
 
-    # ==========================
     # 1. 공격 전 Snapshot 수집
-    # ==========================
     print("[1] 공격 전 AWS 상태 수집 중...")
     snap_before = detector.take_snapshot()
     detector.save_snapshot(snap_before, "snapshot_before.json")
     print("data/snapshot_before.json 저장 완료\n")
 
-    # ==========================
     # 2. 콘솔 공격 대기
-    # ==========================
     input("터미널 콘솔에서 Stratus Red Team 공격을 수행한 후 Enter를 누르세요...")
 
-    # ==========================
     # 3. 공격 후 Snapshot 수집
-    # ==========================
     print("\n[2] 공격 후 AWS 상태 수집 중...")
     snap_after = detector.take_snapshot()
     detector.save_snapshot(snap_after, "snapshot_after.json")
     print("data/snapshot_after.json 저장 완료\n")
 
-    # ==========================
     # 4. Drift 분석 수행
-    # ==========================
     print("[3] 범용 Drift 분석 수행 중...")
     drift_result = detector.compare(snap_before, snap_after)
     
@@ -41,11 +33,8 @@ if __name__ == "__main__":
 
     print("data/drift_result.json 저장 완료\n")
 
-    # ==========================
     # 5. Neo4j 그래프 적재
-    # ==========================
     print("[4] Neo4j 그래프 DB 적재 및 인과관계 매핑 중...")
-    # Neo4j 접속 정보 설정 (본인 환경에 맞게 수정)
     ingestor = UniversalNeo4jDriftIngestor(
         uri="bolt://localhost:7687", 
         user="neo4j", 
